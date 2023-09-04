e36bda057bcf5fbf63b27a4047dff37eb71b6e0b
Ayush Dayal <ayushdayal900@gmail.com>
2023-09-04 11:03:36 +0530

    using break in loop

---
+++ Loops/Break_statement.java
@@ -0,0 +1,17 @@
+package Loops;
+
+public class Break_statement {
+    public static void main(String[] args) {
+        
+    
+    
+    for(int i=1; i<=10; i++)
+    {
+        if(i==3)
+        {break;}
+
+        System.out.println(i);
+
+        }
+    }   
+}
