849cbbf2da02ae0f9418ffc1f96dbbe29d6f7641
Ayush Dayal <ayushdayal900@gmail.com>
2023-09-04 10:01:05 +0530

    100 ties Hello World by using for loop

---
+++ Loops/for_100_times_hello_world.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class for_100_times_hello_world {
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++)
+        {
+            System.out.println("Hello World " + i);
+        }
+    }
+    
+}
