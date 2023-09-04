4f832dd6beac80a76d9f529372c015e50f9d8a7a
Ayush Dayal <ayushdayal900@gmail.com>
2023-09-04 09:19:10 +0530

    Hello World 100 times

---
+++ Loops/hello_world_100.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class hello_world_100 {
+    public static void main(String[] args) {
+        int counter = 1;
+        while(counter<=100) {
+            
+            System.out.println("Hello World");
+            System.out.println(counter);
+
+            counter++;
+            }
+        }
+    }
+
